9fc0df1dbf7f6d37e958a9a2ac997b3c8749572a
Aleksei Rozhnov <alekseyrozh@gmail.com>
2017-11-01 01:23:26 +0000

    minor

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var express = require('express'),
   app = express(),
-  port = process.env.PORT || 3000,
+ // port = process.env.PORT || 3000,
   mongoose = require('mongoose'),
   Notebook = require('./api/models/notebookModels'), //created model loading here
   bodyParser = require('body-parser'),
