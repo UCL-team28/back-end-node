b2ea51784cc4b3b403661d98dda5c6e703aada0f
Aleksei Rozhnov <alekseyrozh@gmail.com>
2017-11-01 21:42:37 +0000

    empty gulp task

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,6 @@
 var gulp = require('gulp');
+   
 
-var exec = require('child_process').exec;
 
-gulp.task('start', function (callback) {
-	    exec('node server/app.js', function (err, stdout, stderr) {
-		            console.log(stdout);
-		            console.log(stderr);
-		            callback(err);
-		        });
-});
+
+gulp.task('empty', function(){});
