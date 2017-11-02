9a0e6686911f70502c7f4e13ec92ed70864e76eb
Marcin Praski <martinp2p@gmail.com>
2017-11-02 14:31:48 +0000

    changed port

--- server.js
+++ server.js
@@ -28,6 +28,6 @@ app.use(function(err, req, res, next) {
   res.json(err.message);
 });
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
+app.listen(80, function () {
+  console.log('Started app on :80')
 })
